634792750a09d68f6a7f7f4bfd2af915518ba11c
Lew Ayotte <lew@ithemes.com>
2015-09-18 10:21:33 -0500

    BuddyPress changed something about how they set the Post ID of their generated pages to 0... instead of keeping the existing post ID. Adding a new filter to get the Post ID earlier so I can deal with this.

--- lib/required-hooks.php
+++ lib/required-hooks.php
@@ -146,13 +146,7 @@ function it_exchange_membership_buddypress_addon_show_version_nag() {
 add_action( 'admin_notices', 'it_exchange_membership_buddypress_addon_show_version_nag' );
 
 /**
- * BuddyPress modifies the Post ID (sets to 0 or -9999) for the non-WordPress pages off of 
- * the Members and Activity and activity pages 
- * (e.g. /members/lew or http://lew.dev.ithemes.com/members/test7/activity/mentions/)
- *
- * So, we hook into template_include, grab the proper $post->ID before they possibly modify it
- * Then we set our own global to keep track of it (and set it later)
- * We also check to see if that page is restricted or dripped per the Exchange Membership functions
+ * We check to see if that page is restricted or dripped per the Exchange Membership functions
  * And we return the $template without modifying at all.
  * 
  * Later we hook into 'the_content', at which point we check to see if the restricted or dripped
@@ -166,20 +160,31 @@ add_action( 'admin_notices', 'it_exchange_membership_buddypress_addon_show_versi
 function it_exchange_membership_buddypress_addon_template_include( $template ) {
 	if ( function_exists( 'is_buddypress' ) && is_buddypress() ) {
 		global $it_exchange_membership_buddypress_addon_is_content_restricted, $it_exchange_membership_buddypress_addon_is_content_dripped, $post, $it_exchange_membership_buddypress_addon_post_id;
-		$it_exchange_membership_buddypress_addon_is_content_restricted = $it_exchange_membership_buddypress_addon_is_content_dripped = false;
-		$it_exchange_membership_buddypress_addon_post_id = $post->ID;
-		
-		if ( it_exchange_membership_addon_is_content_restricted() ) {
-			$it_exchange_membership_buddypress_addon_is_content_restricted = true;
-		}
-					
-		if ( it_exchange_membership_addon_is_content_dripped() ) {
-			$it_exchange_membership_buddypress_addon_is_content_dripped = true;
-		}
+		$post->ID = $it_exchange_membership_buddypress_addon_post_id; //BuddyPress sets this to 0 sometimes... we want it to be the  actual page ID though, which we set in the it_exchange_membership_buddypress_addon_fix_post_id() function.
+		$it_exchange_membership_buddypress_addon_is_content_restricted = it_exchange_membership_addon_is_content_restricted();
+		$it_exchange_membership_buddypress_addon_is_content_dripped = it_exchange_membership_addon_is_content_dripped();
 	}
 	return $template;
 }
 add_filter( 'template_include', 'it_exchange_membership_buddypress_addon_template_include', 1 );
+
+/**
+ * BuddyPress modifies the Post ID (sets to 0 or -9999) for the non-WordPress pages off of 
+ * the Members and Activity and activity pages 
+ * (e.g. /members/lew or http://lew.dev.ithemes.com/members/test7/activity/mentions/)
+ *
+ * So, we hook into wp, grab the proper $post->ID before they possibly modify it
+ * Then we set our own global to keep track of it (and reset it later)
+ *
+ * @since CHANGEME
+ *
+ * @return void
+*/
+function it_exchange_membership_buddypress_addon_fix_post_id() {
+	global $post, $it_exchange_membership_buddypress_addon_post_id;
+	$it_exchange_membership_buddypress_addon_post_id = $post->ID;
+}
+add_filter( 'wp', 'it_exchange_membership_buddypress_addon_fix_post_id', 1 );
 
 /**
  * We hook into 'the_content' (earlier than BuddyPress), so we check to see if the restricted or dripped
@@ -195,7 +200,7 @@ function it_exchange_membership_buddypress_addon_remove_bp_replace_the_content( 
 		global $it_exchange_membership_buddypress_addon_is_content_restricted, $it_exchange_membership_buddypress_addon_is_content_dripped, $post, $it_exchange_membership_buddypress_addon_post_id;
 		
 		if ( $it_exchange_membership_buddypress_addon_is_content_restricted || $it_exchange_membership_buddypress_addon_is_content_dripped ) {
-			$post->ID = $it_exchange_membership_buddypress_addon_post_id; //BuddyPress sets this to 0 sometimes... we want it to be the  actual page ID though, which we set in the it_exchange_membership_buddypress_addon_template_include() function.
+			$post->ID = $it_exchange_membership_buddypress_addon_post_id; //BuddyPress sets this to 0 sometimes... we want it to be the  actual page ID though, which we set in the it_exchange_membership_buddypress_addon_fix_post_id() function.
 			remove_filter( 'the_content', 'bp_replace_the_content' );
 		}
 	}
