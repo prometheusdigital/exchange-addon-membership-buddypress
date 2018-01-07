6cbcbe0edd265234609ca62604ad4be923ec40b2
AJ Morris <aj@ajmorris.me>
2018-01-07 00:07:34 -0500

    New license and update system

--- exchange-addon-membership-buddypress.php
+++ exchange-addon-membership-buddypress.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: ExchangeWP - Membership BuddyPress Add-on
- * Version: 1.2.3
+ * Version: 0.0.1
  * Description: Adds the ExchangeWP Membership management functionality to BuddyPress
  * Plugin URI: https://exchangewp.com/downloads/membership-buddypress/
  * Author: ExchangeWP
@@ -64,38 +64,26 @@ add_action( 'plugins_loaded', 'it_exchange_membership_buddypress_set_textdomain'
  * @param object $updater ithemes updater object
  * @return void
 */
-function ithemes_exchange_addon_membership_buddypress_updater_register( $updater ) {
-	    $updater->register( 'exchange-addon-membership-buddypress', __FILE__ );
+function exchange_membership_buddypress_plugin_updater() {
+
+	$license_check = get_transient( 'exchangewp_license_check' );
+
+	if ($license_check->license == 'valid' ) {
+		$license_key = it_exchange_get_option( 'exchangewp_licenses' );
+		$license = $license_key['exchange_license'];
+
+		$edd_updater = new EDD_SL_Plugin_Updater( 'https://exchangewp.com', __FILE__, array(
+				'version' 		=> '0.0.1', 				// current version number
+				'license' 		=> $license, 		// license key (used get_option above to retrieve from DB)
+				'item_name' 	=> urlencode('Membership BuddyPress'), 	  // name of this plugin
+				'author' 	  	=> 'ExchangeWP',    // author of this plugin
+				'url'       	=> home_url(),
+				'wp_override' => true,
+				'beta'		  	=> false
+			)
+		);
+	}
+
 }
-add_action( 'ithemes_updater_register', 'ithemes_exchange_addon_membership_buddypress_updater_register' );
-// require( dirname( __FILE__ ) . '/lib/updater/load.php' );
 
-if ( ! class_exists( 'EDD_SL_Plugin_Updater' ) )  {
- 	require_once 'EDD_SL_Plugin_Updater.php';
- }
-
- function exchange_membership_buddypress_plugin_updater() {
-
- 	// retrieve our license key from the DB
- 	// this is going to have to be pulled from a seralized array to get the actual key.
- 	// $license_key = trim( get_option( 'exchange_membership_buddypress_license_key' ) );
- 	$exchangewp_membership_buddypress_options = get_option( 'it-storage-exchange_membership_buddypress-addon' );
- 	$license_key = $exchangewp_membership_buddypress_options['membership_buddypress-license-key'];
-
- 	// setup the updater
- 	$edd_updater = new EDD_SL_Plugin_Updater( 'https://exchangewp.com', __FILE__, array(
- 			'version' 		=> '1.2.3', 				// current version number
- 			'license' 		=> $license_key, 		// license key (used get_option above to retrieve from DB)
- 			'item_name' 	=> 'membership-buddypress', 	  // name of this plugin
- 			'author' 	  	=> 'ExchangeWP',    // author of this plugin
- 			'url'       	=> home_url(),
- 			'wp_override' => true,
- 			'beta'		  	=> false
- 		)
- 	);
- 	// var_dump($edd_updater);
- 	// die();
-
- }
-
- add_action( 'admin_init', 'exchange_membership_buddypress_plugin_updater', 0 );
+add_action( 'admin_init', 'exchange_membership_buddypress_plugin_updater', 0 );
