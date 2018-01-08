93405f22e3c905c48bf7fcdcf8f769d393fc20ee
AJ Morris <aj@ajmorris.me>
2018-01-08 16:52:34 -0500

    Updated to use ID instead of Name.

--- exchange-addon-membership-buddypress.php
+++ exchange-addon-membership-buddypress.php
@@ -74,8 +74,8 @@ function exchange_membership_buddypress_plugin_updater() {
 
 		$edd_updater = new EDD_SL_Plugin_Updater( 'https://exchangewp.com', __FILE__, array(
 				'version' 		=> '0.0.1', 				// current version number
-				'license' 		=> $license, 		// license key (used get_option above to retrieve from DB)
-				'item_name' 	=> urlencode('Membership BuddyPress'), 	  // name of this plugin
+				'license' 		=> $license, 				// license key (used get_option above to retrieve from DB)
+				'item_id'		 	=> 591, 					  // name of this plugin
 				'author' 	  	=> 'ExchangeWP',    // author of this plugin
 				'url'       	=> home_url(),
 				'wp_override' => true,
